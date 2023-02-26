cfe93e5b1f3d4e5ab918fbebef4377893d2932a4
Anders <1621-au479641@users.noreply.gitlab.au.dk>
2023-02-26 19:33:49 +0100

    Added donut plot

--- main_gui.py
+++ main_gui.py
@@ -15,12 +15,14 @@
 ===============================================================================
 """ 
  
-import grundfos_library as gl
+import numpy as np
 import tkinter as tk
-from tkinter import ttk, W, LEFT, RIGHT
+from tkinter import ttk, W, LEFT, RIGHT, BOTTOM, TOP
 from PIL import ImageTk, Image
-
-
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+ 
+import AUlibrary as AU
 # =============================================================================
 #  CUSTOM INPUT
 # =============================================================================
@@ -29,6 +31,56 @@ version = "1.1.12-a"
 appname = "DonutMaker 9000"
   
 
+# =============================================================================
+#  Miscellaneous Functions
+# =============================================================================
+
+def calculate_torus(n_pts, c, a):
+    """
+    ---------------------------------------------------------------------------
+    | Caculate the points that make up a donut/torus                          |
+    ---------------------------------------------------------------------------
+    |  INPUTS:
+    |     n_pts (int) : Number of points to plot
+    |     c (float) : 
+    |     a (float) : 
+    |_________________________________________________________________________|
+    """  
+    U = np.linspace(0, 2*np.pi, n_pts)
+    V = np.linspace(0, 2*np.pi, n_pts)
+    U, V = np.meshgrid(U, V)
+    X = (c+a*np.cos(V))*np.cos(U)
+    Y = (c+a*np.cos(V))*np.sin(U)
+    Z = a*np.sin(V)
+    
+    return X, Y, Z  
+
+
+def add_donut_plot(widget=None):
+    """
+    ---------------------------------------------------------------------------
+    | Plots a donut. Well technically its a torus, but donut sounds better.   |
+    | If a tkinter widget is supplied as input, it will be packed onto        |
+    | that widget. If nothing is supplied it will plot as normally.           |
+    ---------------------------------------------------------------------------
+    """
+    x, y, z = calculate_torus(100, 1, 1)
+    
+    fig = plt.Figure(figsize=(3,3), dpi=100)
+    ax = fig.add_subplot(projection='3d')
+    if widget != None:
+        figcanvas = FigureCanvasTkAgg(fig, widget)
+    ax.plot_surface(x, y, z, antialiased=True, color=AU.AUpink)
+    ax.set_title('Donut Plot') 
+    ax.set_aspect('equal', adjustable='box')
+ #   ax.patch.set_alpha(0.5)
+    
+    if widget == None:
+        plt.show()
+    else:
+        return figcanvas, ax
+    
+    
 # =============================================================================
 #  Splash Screen
 # =============================================================================
@@ -59,17 +111,18 @@ class SplashScreen(tk.Tk):
         self.image = tk.PhotoImage(file=self.image_file_path)                  # Loading the splash image
         self.x = int((self.width / 2) - (self.image.width() / 2))
         self.y = int((self.height / 2) - (self.image.height() / 2))
-        self.image_geometry = '{}x{}+{}+{}'.format(self.image.width() - 5, 
-                                                   self.image.height() - 5, 
+        self.image_geometry = '{}x{}+{}+{}'.format(self.image.width() , 
+                                                   self.image.height() , 
                                                    self.x, self.y)
         self.geometry(self.image_geometry)
         self.canvas = tk.Canvas(self, width=self.image.width(), 
-                                      height=self.image.height())
+                                      height=self.image.height(),
+                                      highlightthickness=0)
         self.canvas.pack()
         self.canvas.create_image(0, 0, image=self.image, anchor='nw')
         self.after(self.display_time, self.destroy)                            # Destroy window after time as expired
        
-
+        self.update()
 # =============================================================================
 #  CONFIGURE MAIN FRAME AND STYLES
 # =============================================================================
@@ -92,8 +145,8 @@ class MainApplication(tk.Tk):
         # Stuff to configure:
         self.title(self.title_string)                                          # Specifying the title of the window
         self.iconbitmap('./imgs/icon.ico')                                     # Adding our own icon to the app
-        self.w = 760                                                           # width for the Tk root
-        self.h = 600                                                           # height for the Tk root   
+        self.w = 600                                                           # width for the Tk root
+        self.h = 780                                                           # height for the Tk root   
         
         # Calculating frame dimensions
         self.ws = self.winfo_screenwidth()                                     # width of the screen
@@ -110,14 +163,19 @@ class MainApplication(tk.Tk):
         
         
     def configure_gui_style(self):
+        """
+        -----------------------------------------------------------------------
+        |  Method for configuring the overall style of the gui                |
+        -----------------------------------------------------------------------
+        """
         self.style = ttk.Style(self)                                           # Create a style
         self.tk.call('source', './azure_theme/azure.tcl')                      # Import the tcl file 
         self.tk.call("set_theme", "light")                                     # Possible values for the azure tcl is either "light" or "dark"
         self.style.configure('.', font=('AU Passata', 12, 'normal'))           # Configuring the normal font 
-        self.style.configure('TButton', background=gl.RGBtoHex(gl.Blue_1), 
+        self.style.configure('TButton', background=AU.RGBtoHex(AU.AUblue1), 
                              foreground='white', width=10, borderwidth=10,
                              focusthickness=100, 
-                             focuscolor=gl.RGBtoHex(gl.Grundfos_lightblue), 
+                             focuscolor=AU.RGBtoHex(AU.AUlightblue), 
                              font=('AU Passata', 10, 'bold'))
         
         
@@ -126,43 +184,33 @@ class MainApplication(tk.Tk):
        #  CREATE FRAMES
        # ======================================================================
     
-        self.main_frame = ttk.LabelFrame(self, text='Create Custom Product')
-        self.main_frame.grid(row=0, column=0, sticky=W, padx=20, pady=20)
+        self.switch = ttk.Checkbutton(
+            self, text="Theme", style="Switch.TCheckbutton", command=self.change_theme
+        )
+    
+    
+        self.display_frame = ttk.LabelFrame(self, text='Custom Donut')
+  
       
-        class create_description_subframe:
-      
-            self.descrip_frame = ttk.LabelFrame(self.main_frame, text='Description')
-            self.descrip_frame.grid(row=0, column=0, sticky=W, padx=5, pady=2, columnspan=4)
-           
-            self.descript = "Input volume flow rate, Q, and Head height, H, " \
-                            "\nand create one ore more corresponding CAD-files "\
-                            "based on the scaling laws, " \
-                            "\nusing the UMS-20-20 centrifugal pump as a base\n" \
-                            "\n" \
-                            "Input a either a single value in Q and H or a \n" \
-                            "series of values to create multiple products, eg.\n" \
-                            "Q: 1, 2, 4 \n" \
-                            "H: 1.5, 2.5, 5\n" \
-                            "\n" \
-                            "Output CAD-files are saved in a folder with the format:\n" \
-                            "ProductType_Q_Q-value_H_H-value"
-                            
-            self.descript_label = tk.Label(self.descrip_frame, text= self.descript,
-                                   anchor='w', justify=LEFT, font=('AU Passata', 9, 'normal'))
-            self.descript_label.grid(row=0, column=0, sticky=W, padx=5, pady=10)
-       
-        create_description_subframe()
-        
-        
-        
-       # ======================================================================
-       #  CREATE FRAMES
-       # ======================================================================
-       
-        self.product_label = ttk.Label(self.main_frame, text="Choose Product:", anchor='e', justify=RIGHT,
-                                 font=('AU Passata', 10, 'normal'))
+        self.topframe = ttk.Frame(self.display_frame)
+
+        self.panel = ttk.Label(self.topframe) 
+
+        self.figcanvas, self.ax = add_donut_plot(self.topframe)
  
-        self.panel = ttk.Label(self.main_frame) 
+        
+        self.widget_main_frame = ttk.Frame(self)
+        self.product_parm_frame = ttk.LabelFrame(self.widget_main_frame, 
+                                                 text='Product Parameters')    # Frame for the product parameters
+        self.donut_parm_frame = ttk.LabelFrame(self.widget_main_frame, 
+                                               text='Donut Parameters')        # Frame for the product parameters
+        
+        self.product_label = ttk.Label(self.product_parm_frame, 
+                                       text="Choose Product:", 
+                                       anchor='e', justify=RIGHT,
+                                       font=('AU Passata', 10, 'normal')
+                                       )
+
         
         self.choice_img_dict = {"Pink Donut":"./imgs/donut_pink.png", 
                                 "Green Donut": "./imgs/donut_green.png",
@@ -170,48 +218,32 @@ class MainApplication(tk.Tk):
                                 "White Donut": "./imgs/donut_white.png",
                                 }                                              # Dictionary for our dropdown choices as keys and present image as value
         
-        self.prd_img_width = 300
+        self.prd_img_width = 200
         self.v = tk.StringVar()
         self.v.trace('w', self.on_field_change)
-        self.Product_range = ttk.Combobox(self.main_frame, textvar=self.v, 
+        self.Product_range = ttk.Combobox(self.product_parm_frame, textvar=self.v, 
                                           values=list(self.choice_img_dict.keys()))
         self.Product_range.current(0)
         
-        self.Q_label = ttk.Label(self.main_frame, text="Flow (Q):", anchor='e', 
+        self.c_label = ttk.Label(self.donut_parm_frame, text="c :", anchor='e', 
                                  justify=RIGHT, font=('AU Passata', 10, 'bold'))
-        self.Q_value = tk.StringVar()
-        self.Q_entry = ttk.Entry(self.main_frame, textvariable=self.Q_value, width=30)
-        self.Q_value.set('1')
-        self.Q_unit = ttk.Label(self.main_frame, text="[m\u00b3/s]", anchor='w', 
-                                justify=LEFT, font=('AU Passata', 10, 'normal'))
-        self.H_label = ttk.Label(self.main_frame, text="Head (H):", anchor='e', 
+        self.c_value = tk.StringVar()
+        self.c_spinbox = ttk.Spinbox(self.donut_parm_frame, from_=0, to=100, increment=0.1, state="readonly")
+        self.c_spinbox.insert(0, 0.5)     
+        self.c_value.set(1.0)
+
+        self.a_label = ttk.Label(self.donut_parm_frame, text="c :", anchor='e', 
                                  justify=RIGHT, font=('AU Passata', 10, 'bold'))
-        self.H_value = tk.StringVar()
-        self.H_entry = ttk.Entry(self.main_frame, textvariable=self.H_value, 
-                                 width=30)
-        self.H_value.set('1.5')
-        self.H_unit = ttk.Label(self.main_frame, text="[m]", anchor='w', 
-                                justify=LEFT, font=('AU Passata', 10, 'normal'))
-         
-   
-        self.destination_label = ttk.Label(self.main_frame, text="Destination:",
-                                           anchor='e', justify=RIGHT,
-                                           font=('AU Passata', 10, 'normal'))
-        self.destination_value = tk.StringVar()
-        self.destination_entry = ttk.Entry(self.main_frame, 
-                                           textvariable=self.destination_value, 
-                                           width=70)
-     
-         
-        self.main_button = ttk.Button(self.main_frame, text="Create stuff!", 
+        self.a_value = tk.StringVar()
+        self.a_spinbox = ttk.Spinbox(self.donut_parm_frame, from_=0, to=100, increment=0.1, state="readonly")
+        self.a_spinbox.insert(0, 0.5)     
+        self.a_value.set(1.0)
+      
+        self.main_button = ttk.Button(self.widget_main_frame, text="Update Donut!", 
                                       style="Accent.TButton",
                                       width=50, command=self.button_action)
-        
-        self.suc_mes = tk.StringVar()
-        self.suc_mes.set(' ')
-        self.success_message = tk.Label(self.main_frame, textvariable=self.suc_mes,
-                                        anchor='w', justify=LEFT,
-                                        font=('AU Passata', 10, 'normal'))
+ 
+  
     
     def define_widget_layout(self): 
         """
@@ -219,19 +251,27 @@ class MainApplication(tk.Tk):
         | Method for defining the widget layouts                              |
         -----------------------------------------------------------------------
         """
-        self.panel.grid(row=0, column=4, sticky=W, padx=25, pady=25, rowspan=1)
-        self.product_label.grid(row=1, column=0, sticky=W, padx=5, pady=5)
-        self.Product_range.grid(row=1, column=1, sticky=W, padx=5, pady=30, columnspan=2)
-        self.Q_label.grid(row=2, column=0, sticky=W, padx=5, pady=5)
-        self.Q_entry.grid(row=2, column=1, sticky=W, padx=5, pady=5)
-        self.Q_unit.grid(row=2, column=2, sticky=W, padx=5, pady=5)
-        self.H_label.grid(row=3, column=0, sticky=W, padx=5, pady=5)
-        self.H_entry.grid(row=3, column=1, sticky=W, padx=5, pady=5)
-        self.H_unit.grid(row=3, column=2, sticky=W, padx=5, pady=5)
-        self.destination_label.grid(row=4, column=0, sticky=W, padx=25, pady=25, columnspan=4)
-        self.destination_entry.grid(row=4, column=1, sticky=W, padx=25, pady=25, columnspan=4) 
-        self.main_button.grid(row=5, column=1, sticky=W, padx=25, pady=25, columnspan=4)
+        self.switch.pack( side = TOP, pady=25 )                                # Child of self 
         
+        self.display_frame.pack( side = TOP )                                  # Child of self 
+        self.topframe.pack( side = TOP )                                       # Child of self.display_frame
+
+        self.panel.pack( side = LEFT )                                         # Child of self.topframe which is a child of self.display_frame
+        self.figcanvas.get_tk_widget().pack( side = LEFT)                      # Child of self.topframe which is a child of self.display_frame
+        self.widget_main_frame.pack( side = TOP )                              # Child of self 
+        self.product_parm_frame.pack( side = TOP, fill=tk.BOTH, pady=15)       # Child of self.widget_main_frame
+        self.donut_parm_frame.pack( side = TOP, fill=tk.BOTH , pady=15)        # Child of self.widget_main_frame
+        
+        self.product_label.pack( side = LEFT, padx=25, pady=25)                # Child of self.product_parm_frame
+        self.Product_range.pack( side = LEFT, padx=25, pady=25)                # Child of self.product_parm_frame
+        
+        self.c_label.pack( side = LEFT, padx=15, pady=25)                      # Child of self.donut_parm_frame
+        self.c_spinbox.pack( side = LEFT, padx=15, pady=25)                    # Child of self.donut_parm_frame
+        self.a_label.pack( side = LEFT, padx=15, pady=25)                      # Child of self.donut_parm_frame
+        self.a_spinbox.pack( side = LEFT, padx=15, pady=25)                    # Child of self.donut_parm_frame
+ 
+        self.main_button.pack(  padx=25, pady=25, side = BOTTOM) 
+ 
         
     def change_theme(self):
         """
@@ -260,11 +300,37 @@ class MainApplication(tk.Tk):
         scaling = self.prd_img_width / product_image.size[0]
         img2 = ImageTk.PhotoImage(product_image.resize((self.prd_img_width, 
                                                         int(product_image.size[1] * scaling)), 
-                                                       Image.ANTIALIAS))
+                                                        Image.Resampling.LANCZOS))
         self.panel.configure(image=img2)
         self.panel.image = img2
         
- 
+        
+    def update_donut_plot(self):
+        
+        colorchoice = self.Product_range.get()
+        
+        if colorchoice.startswith("Pink"):
+            plotcolor = AU.AUpink
+        elif colorchoice.startswith("Green"):
+            plotcolor = AU.AUgreen
+        elif colorchoice.startswith("Blue"):   
+            plotcolor = AU.AUlightblue
+        elif colorchoice.startswith("White"):
+            plotcolor = AU.AUgrey
+        else:
+            plotcolor = AU.taskegul
+        
+        
+        a = float(self.a_spinbox.get())
+        c = float(self.c_spinbox.get())
+        x, y, z = calculate_torus(100, a, c)
+        self.ax.clear()                                                        # clear axes from previous plot
+        self.ax.plot_surface(x, y, z, antialiased=True, color=plotcolor)
+        self.ax.set_title('Donut Plot') 
+        self.ax.set_aspect('equal', adjustable='box')
+        self.figcanvas.draw()
+        
+        
     def button_action(self):
         """
         -----------------------------------------------------------------------
@@ -275,12 +341,15 @@ class MainApplication(tk.Tk):
         
         try:
             # < Method for button execution here >
-            self.suc_mes.set('Operation succeeded!')
-            self.success_message.config(fg='green')
+            self.update_donut_plot()
+                
+      #      self.suc_mes.set('Operation succeeded!')
+      #      self.success_message.config(fg='green')
             
         except Exception as e:    
-            self.suc_mes.set(e)
-            self.success_message.config(fg='red')
+            print(e)
+       #     self.suc_mes.set(e)
+       #     self.success_message.config(fg='red')
         
  
         
